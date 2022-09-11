caf88c5a218d042a3d5643936cfc9e3cca4dd7bc
Mariusz Krawczyk <przyslony@gmail.com>
2022-09-11 23:55:34 +0100

    first commit create back end install keystone lib, create user shame, create keystone with auth

---
+++ keystone.ts
@@ -0,0 +1,64 @@
+import 'dotenv/config';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import { createAuth } from '@keystone-next/auth';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
+import { sendPasswordResetEmail } from './lib/mail';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keyatone-sick-fits-toturial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360,
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+  },
+  passwordResetLink: {
+    async sendToken(args) {
+      console.log(args);
+      await sendPasswordResetEmail(args.token, args.identity);
+    },
+  },
+});
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      async onConnect(keystone) {
+        console.log('connected to data base!');
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
+    },
+    lists: createSchema({
+      User,
+    }),
+
+    ui: {
+      isAccessAllowed: ({ session }) => !!session?.data,
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: 'id name',
+      Product: 'id name',
+    }),
+  })
+);
